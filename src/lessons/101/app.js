36b50ccdc6c565f45e58b2eafa573d2ed045b08f
Vishwajeet Raj <ext-vishwajeet.raj@graphy.com>
2023-10-02 13:19:28 +0530

    Static to Dynamic Import
    Changing the way we import components and adding Suspense boundary to handle promises.

--- src/lessons/101/app.js
+++ src/lessons/101/app.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { Fragment, Suspense, lazy } from "react";
 // 1. Change this static import to a dynamic import, wrapped in React.lazy
-import PokemonDetail from "./pokemon-detail";
+// import PokemonDetail from "./pokemon-detail";
+
+const PokemonDetail = lazy(() => import("./pokemon-detail"));
 
 export default function App() {
   return (
     <div>
       {/* 2. Wrap this component in a React.Suspense component with fallback */}
-      <PokemonDetail />
+     <Suspense fallback={<Fragment>Loading...</Fragment>}>
+     <PokemonDetail />
+     </Suspense>
     </div>
   );
 }
